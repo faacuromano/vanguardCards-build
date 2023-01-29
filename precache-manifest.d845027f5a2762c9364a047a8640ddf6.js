28d79b2fd6f6ce285eb39d48e39013bae079365a
Facundo Romano <facundo-romano@hotmail.com>
2023-01-29 15:41:57 -0300

    Add files via upload

---
+++ precache-manifest.d845027f5a2762c9364a047a8640ddf6.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "84f0c281bffcfcddc529",
+    "url": "/static/css/main.26b56773.chunk.css"
+  },
+  {
+    "revision": "84f0c281bffcfcddc529",
+    "url": "/static/js/main.84f0c281.chunk.js"
+  },
+  {
+    "revision": "ca3eae930b8007075bbe",
+    "url": "/static/css/1.63d7de9c.chunk.css"
+  },
+  {
+    "revision": "ca3eae930b8007075bbe",
+    "url": "/static/js/1.ca3eae93.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5da3627cb48fe7cbc27e300163507922",
+    "url": "/static/media/pngegg.5da3627c.png"
+  },
+  {
+    "revision": "335d94b71a38c554062402fb9e9516d0",
+    "url": "/static/media/vcard1.335d94b7.jpg"
+  },
+  {
+    "revision": "e47c386cc501fbace3655e4b23baac64",
+    "url": "/static/media/EIFCARD.e47c386c.jpg"
+  },
+  {
+    "revision": "d658ac918d8b4ebe50513efc3a4e89eb",
+    "url": "/static/media/eeee.d658ac91.JPG"
+  },
+  {
+    "revision": "95b2c6bb05776c12097022c334e0980e",
+    "url": "/static/media/momocho.95b2c6bb.jpg"
+  },
+  {
+    "revision": "cf7910d20740c63538ec946df3897795",
+    "url": "/static/media/V_Card_2.cf7910d2.png"
+  },
+  {
+    "revision": "6d73759f5301ae45f5bb5aaff347ba22",
+    "url": "/static/media/the_v_king.6d73759f.png"
+  },
+  {
+    "revision": "13aa4e9fe9f700e07ab01f2b360ad9a6",
+    "url": "/static/media/MorlacoYKAppa.13aa4e9f.png"
+  },
+  {
+    "revision": "0729e053fa53cfe568fb4f6e271a5ed4",
+    "url": "/static/media/the_v_card_wizard.0729e053.png"
+  },
+  {
+    "revision": "bf67e7837c8ce03af48611d8d1a93643",
+    "url": "/static/media/V_Card_Final.bf67e783.png"
+  },
+  {
+    "revision": "0c65814f2b29b9078bcc4bd1835fae64",
+    "url": "/static/media/XXFUXXVAMONOSPEST.0c65814f.png"
+  },
+  {
+    "revision": "2000a04a4289c215db390dbab28f82b0",
+    "url": "/static/media/Odinson.2000a04a.ttf"
+  },
+  {
+    "revision": "2970d694699c396bd03658e323583f7f",
+    "url": "/static/media/asd1.2970d694.jpg"
+  },
+  {
+    "revision": "0f3b8864e0ab017463f5020229c5b5a9",
+    "url": "/index.html"
+  }
+];
